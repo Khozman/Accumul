d2e02244004810a3f3e438489bdd21e824ea67e3
Khozman <bhekinkosikhosa@gmail.com>
2019-10-04 12:24:05 +0200

    adding my Accumul project

---
+++ Accumul.java
@@ -0,0 +1,27 @@
+package kata.Accumul;
+
+public class Accumul {
+    public static void main(String[] args) {
+        String str = "Lunga";
+        String strLower = str.toLowerCase();
+        int numberOfChar = str.length();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < numberOfChar; i++){
+            for(int j = 0; j < numberOfChar; j++){
+                if(j <= i) {
+                    if(j == 0){
+                        sb.append(strLower.toUpperCase().charAt(i));
+                    } else {
+                        sb.append(strLower.charAt(i));
+                    }
+                }
+            }
+            if(i != numberOfChar-1) {
+                sb.append("-");
+            }
+        }
+        System.out.println(sb);
+    }
+}
